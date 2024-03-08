d2f3c08969513bc94461479a39f5db034b8b8cf1
GamjaJEON <indian_potato@naver.com>
2024-03-08 17:13:23 +0900

    feat: swiper 구현

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,13 @@
 import { styled } from 'styled-components'
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Nav = () => {
   const [show, setShow] = useState('false')
+
+  const [searchValue, setSearchValue] = useState('')
+
+  const navigate = useNavigate()
 
   const listener = () => {
     if (window.scrollY > 50) {
@@ -19,6 +24,11 @@ const Nav = () => {
     }
   }, [])
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value)
+    navigate(`/search?q=${e.target.value}`)
+  }
+
   return (
     <NavWrapper show={show}>
       <Logo>
@@ -28,9 +38,45 @@ const Nav = () => {
           onClick={() => (window.location.href = '/')}
         />
       </Logo>
+
+      <Input
+        type="text"
+        className="nav__input"
+        value={searchValue}
+        onChange={handleChange}
+        placeholder="영화를 검색해주세요."
+      />
+
+      <Login>로그인</Login>
     </NavWrapper>
   )
 }
+
+const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 5px;
+  padding: 5px;
+  border: 1px solid lightgray;
+`
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`
 
 const NavWrapper = styled.nav`
   position: fixed;
