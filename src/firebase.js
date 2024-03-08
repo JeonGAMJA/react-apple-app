145694c1a21eae107caa5dca20907333c9d2762d
GamjaJEON <indian_potato@naver.com>
2024-03-09 00:25:57 +0900

    feat: 로그인버튼 구현

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAldzXbRt7D9ng6t2YEg9Cl_o6DWY9xojI',
+  authDomain: 'react-movie-24158.firebaseapp.com',
+  projectId: 'react-movie-24158',
+  storageBucket: 'react-movie-24158.appspot.com',
+  messagingSenderId: '815206978939',
+  appId: '1:815206978939:web:269e11e5172ff26ce899de',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
